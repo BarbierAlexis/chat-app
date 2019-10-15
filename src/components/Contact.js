0369bd76867fee8cbefbff73c4d48b30e43b2a82
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-10-15 13:54:33 +0200

    Add state to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,44 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact(props) {
-  return (
+class Contact extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.status,
+    };
+  }
+  render(){
+    return (
     <div className = 'Contact'>
-      <img className ="avatar" alt='avatar' src={props.avatar}/>
+      <img className ="avatar" alt='avatar' src={this.props.avatar}/>
       <div className = 'Card'>
-        <p className ='name'>{props.name}</p>
-        <div className ='status'>{props.status? <div className ="card-display"><p className ="status-online"></p><p className ="status-text">Online </p></div> : <div className ="card-display"><p className ="status-offline"></p><p className ="status-text">Offline </p></div>}
+        <p className ='name'>{this.props.name}</p>
+        <div className ='status'>{this.state.online? 
+          <div className ="card-display">
+            <p 
+            className ="status-online"
+            onClick={event => {
+              const newOnline = !this.state.online;
+              this.setState({online: newOnline });
+              console.log(this.state.online )
+            }}></p>
+            <p className ="status-text">Online </p>
+          </div> : 
+          <div className ="card-display">
+            <p className ="status-offline" onClick={event => {
+              const newOnline = !this.state.online;
+              this.setState({online: newOnline });
+              console.log(this.state.online )
+            }}></p>
+            <p className ="status-text">Offline </p>
+          </div>}
         </div>
       </div>
     </div>
-  )
+    )
+  }
 }
+  
 
 export default Contact;
